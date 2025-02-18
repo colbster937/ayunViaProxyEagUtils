f28059bb85a3942de88246df90c4ba8d9771160b
Colbster937 <colbychittenden@gmail.com>
2025-02-17 21:30:12 -0600

    funny commit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# ayunViaProxyEagUtils
+this is a forked version of ayunami2000's ayunviaproxyeagutils with support for newer viaproxy versions.
+
+## ORIGINAL README:
+### ayunViaProxyEagUtils
 eaglercraft support for viaproxy. ws:// on same port as java. supports both 1.5.2 and 1.8.8 eaglercraft depending on the configuration (e.g. use legacy passthrough & protocolsupport if the backend server doesn't support 1.5.2)
 
 note: skin files are excluded
--- src/main/java/me/ayunami2000/ayunViaProxyEagUtils/FunnyConfig.java
+++ src/main/java/me/ayunami2000/ayunViaProxyEagUtils/FunnyConfig.java
@@ -4,7 +4,6 @@ import com.viaversion.viaversion.util.Config;
 
 import java.io.File;
 import java.net.URL;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -15,37 +14,31 @@ public class FunnyConfig extends Config {
     public static boolean eaglerVoice = true;
     public static int eaglerServerMode = 0;
 
-    protected FunnyConfig(File configFile) {
+    public FunnyConfig(File configFile) {
         super(configFile, Logger.getLogger("FunnyConfig"));
+        reload();
     }
 
     @Override
     public URL getDefaultConfigURL() {
-        return Main.class.getResource("/vpeagutils.yml");
+        return getClass().getClassLoader().getResource("vpeagutils.yml");
     }
 
     @Override
-    protected void handleConfig(Map<String, Object> map) {
-        Object item = map.get("premium-skins");
-        if (item instanceof Boolean) {
-            premiumSkins = (Boolean) item;
+    protected void handleConfig(Map<String, Object> config) {
+        if (config == null || getValues() == null) {
+            logger.severe("Config is null! This should not happen.");
+            return;
         }
-        item = map.get("eagler-skins");
-        if (item instanceof Boolean) {
-            eaglerSkins = (Boolean) item;
-        }
-        item = map.get("eagler-voice");
-        if (item instanceof Boolean) {
-            eaglerVoice = (Boolean) item;
-        }
-        item = map.get("eagler-server-mode");
-        if (item instanceof Integer) {
-            eaglerServerMode = (Integer) item;
-        }
+
+        premiumSkins = getBoolean("premium-skins", premiumSkins);
+        eaglerSkins = getBoolean("eagler-skins", eaglerSkins);
+        eaglerVoice = getBoolean("eagler-voice", eaglerVoice);
+        eaglerServerMode = getInt("eagler-server-mode", eaglerServerMode);
     }
 
     @Override
     public List<String> getUnsupportedOptions() {
-        return Collections.emptyList();
+        return List.of();
     }
 }
